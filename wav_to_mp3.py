35b6593176acf3ce45d0fafd6469caf46304ab38
eylonK14 <eylony1403@gmail.com>
2025-03-26 19:04:27 +0200

    changed

--- wav_to_mp3.py
+++ wav_to_mp3.py
@@ -1,3 +1,3 @@
 from pydub import AudioSegment
 
-AudioSegment.from_wav("/input/file.wav").export("/output/file.mp3", format="mp3")
+AudioSegment.from_wav("/home/eylon/gqrx_20250326_165603_97500000.wav").export("/home/eylon/gqrx_20250326_165603_97500000.mp3", format="mp3")
